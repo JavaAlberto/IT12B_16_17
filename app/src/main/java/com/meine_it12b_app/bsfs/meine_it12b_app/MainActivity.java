3cdf8811c28d33053ec2297de62112c6af305ee6
pfleiderer albrecht <Pfleiderer@berufsschule-freising.de>
2016-10-04 12:00:11 +0200

    Initial commit

---
+++ app/src/main/java/com/meine_it12b_app/bsfs/meine_it12b_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.meine_it12b_app.bsfs.meine_it12b_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
